7f812e82be669e95bd555937fa666aa5abbd617a
Oscar Del Rio <odelrio@servicios.mc4.com.bo>
2025-06-19 18:31:41 -0400

    change dir api

--- api/auth/login/route.ts
+++ api/auth/login/route.ts
@@ -2,7 +2,7 @@ export const dynamic = 'force-dynamic'
 export const runtime = 'nodejs'
  
 import { NextResponse } from 'next/server'
-import { loginUser } from '../../../../../services/authService'
+import { loginUser } from '../../../services/authService'
 import { serialize } from 'cookie'
 
 export async function POST(request: Request) {
