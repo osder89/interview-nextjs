f273bb7bf936c97d4d0330bd7375d38f7753066b
Oscar Del Rio <odelrio@servicios.mc4.com.bo>
2025-06-19 19:45:24 -0400

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['pg', 'pg-hstore', 'sequelize'],
   redirects: async () => [
     {
       source: '/',
