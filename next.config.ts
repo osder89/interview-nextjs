151ac956c7d31de30762b4b25529ab3f63a4867e
Oscar Del Rio <odelrio@servicios.mc4.com.bo>
2025-06-19 19:40:51 -0400

    fix pg

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }): any => {
-    if (isServer) {
-      config.externals = config.externals || []
-      config.externals.push(
-        (
-          { request }: { request: string },
-          callback: (error?: Error | null, result?: string) => void
-        ) => {
-          if (/^(pg|pg-hstore|sequelize)$/.test(request)) {
-            return callback(null, `commonjs ${request}`)
-          }
-          callback()
-        }
-      )
-    }
-    return config
-  },
   redirects: async () => [
     {
       source: '/',
